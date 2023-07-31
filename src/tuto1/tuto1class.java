6506179a618f122298770da3b6f3e5420065d6ec
Indhumathi_n <indhumathinallasamy33@gmail.com>
2023-07-31 13:16:10 +0530

    my 1st commit

---
+++ src/tuto1/tuto1class.java
@@ -0,0 +1,15 @@
+package tuto1;
+
+public class tuto1class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		int b=20;
+		int c=a+b;
+		System.out.println(c);
+		System.out.println("cloneed and updated");
+
+	}
+
+}
